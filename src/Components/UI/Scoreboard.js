a4cb76a80ac6f98cfad8a0f6cc7decac2dc95422
Oleksii Kriuchkov <ahookof@gmail.com>
2022-06-02 00:49:12 +0300

    fix

---
+++ src/Components/UI/Scoreboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const scoreboard = ({ children, curName }) => {
+    return (
+        <div className='d-flex'>
+            <span> 1 {curName} = </span>
+            <div className='rate mx-2' > {children} </div>
+            <span > UAH </span>
+        </div>
+    )
+}
+
+export default scoreboard
--- src/Hooks/useFetchRate.js
+++ src/Hooks/useFetchRate.js
@@ -4,25 +4,25 @@ import { useRecoilState } from 'recoil';
 import { isErrorState, errorMsgState } from '../Atoms/atomError';
 import { useSetCurrencyArray } from './useSetCurrencyArray';
 
- function useFetchRate(url) {
-    const [responce, setResponce] = useState({});
+function useFetchRate(url) {
+    // const [responce, setResponce] = useState({});
     const [isError, setIsError] = useRecoilState(isErrorState);
     const [errorMsg, setErrorMsg] = useRecoilState(errorMsgState)
 
     const { createCurrencyArray } = useSetCurrencyArray();
 
-    useEffect(async () => {
+    useEffect(async() => {
         await axios.get(url)
-        .then((responce) => {
-            setIsError(false)
-            createCurrencyArray(responce.data);
-        })
-        .catch((error) => {
-            setIsError(true)
-            setErrorMsg(error.toJSON().message)
-        })
+            .then((responce) => {
+                setIsError(false)
+                createCurrencyArray(responce.data);
+            })
+            .catch((error) => {
+                setIsError(true)
+                setErrorMsg(error.toJSON().message)
+            })
     }, [])
 
-    return { errorMsg }
+    // return { errorMsg }
 }
 export default useFetchRate
